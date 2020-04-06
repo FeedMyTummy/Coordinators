0acb28d641ba63febca2a2e7f5543822974e1b9f
FeedMyTummy <nick.untethered@icloud.com>
2020-04-06 17:36:31 -0400

    .

---
+++ Coordinators/Services/Databases/MockGetRestaurantsFailureDatabase.swift
@@ -0,0 +1,31 @@
+//
+//  MockGetRestaurantsFailureDatabase.swift
+//  Coordinators
+//
+//  Created by Nicolas Silva on 4/6/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+final class MockGetRestaurantsFailureDatabase: DatabaseService {
+    
+    private var _isLoggedIn = AuthenticationStatus.loggeOut
+    var isLoggedIn: AuthenticationStatus { _isLoggedIn }
+    static let shared = MockGetRestaurantsFailureDatabase()
+    
+    private init() { /* EMPTY */ }
+    
+    func getRestaurants(_ completion: @escaping (Result<[Restaurant], Error>) -> Void) {
+        completion(.failure(DataBaseError.unknown))
+    }
+    
+    func login(_ completion: @escaping (Result<Void, Error>) -> Void) {
+        completion(.success(()))
+    }
+    
+    func logout(_ completion: @escaping (Result<Void, Error>) -> Void) {
+        completion(.success(()))
+    }
+    
+}
